994bacabb6a97ab2bd68089638867fd548d2e904
RRoggia <renanzr@gmail.com>
2019-05-14 23:56:57 -0300

    add exercise6

---
+++ src/main/java/com/hackerrank/java/introduction/Exercise6.java
@@ -0,0 +1,29 @@
+package com.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise6.txt");
+		
+		try (Scanner scan = new Scanner(System.in);) {
+			int t = scan.nextInt();
+			for (int i = 0; i < t; i++) {
+				int a = scan.nextInt();
+				int b = scan.nextInt();
+				int n = scan.nextInt();
+
+				int result = a;
+
+				for (int j = 0; j <= n - 1; j++) {
+					result += (int) Math.pow(2, j) * b;
+					System.out.print(result + " ");
+				}
+				System.out.println();
+			}
+		}
+
+	}
+
+}
