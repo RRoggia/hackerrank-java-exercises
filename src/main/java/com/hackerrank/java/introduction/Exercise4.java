c037fc1e6373f115b811327255b9a7198341b480
RRoggia <renanzr@gmail.com>
2019-05-14 23:30:48 -0300

    add exercise 4

---
+++ src/main/java/com/hackerrank/java/introduction/Exercise4.java
@@ -0,0 +1,29 @@
+package com.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class Exercise4 {
+	
+	/*
+	 * In each line of output there should be two columns: 
+	 * The first column contains the String and is left justified using exactly 15 characters. 
+	 * The second column contains the integer, expressed in exactly 3 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
+	 */
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise4.txt");
+
+		try (Scanner scan = new Scanner(System.in);) {
+			System.out.println("================================");
+			for (int i = 0; i < 3; i++) {
+				String s1 = scan.next();
+				int x = scan.nextInt();
+
+				System.out.format("%-15s%03d", s1, x);
+				System.out.println();
+			}
+			System.out.println("================================");
+
+		}
+	}
+}
