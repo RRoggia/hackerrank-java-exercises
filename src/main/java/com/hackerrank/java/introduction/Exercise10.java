b41373dbc07caeb6b6cd544c99489450a0e4c8c5
RRoggia <renanzr@gmail.com>
2019-05-15 18:52:05 -0300

    add exercise 10

---
+++ src/main/java/com/hackerrank/java/introduction/Exercise10.java
@@ -0,0 +1,22 @@
+package com.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class Exercise10 {
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise10.txt");
+
+		try (Scanner scan = new Scanner(System.in)) {
+			int value = scan.nextInt();
+
+			String s = Integer.toString(value);
+
+			if (value == Integer.parseInt(s)) {
+				System.out.println("Good Job");
+			} else {
+				System.out.println("Wrong answer.");
+			}
+		}
+	}
+}
