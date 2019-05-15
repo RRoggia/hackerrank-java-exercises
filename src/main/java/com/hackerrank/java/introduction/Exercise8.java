d8144882255087a9d429ce5d1a804bc8d7ef6722
RRoggia <renanzr@gmail.com>
2019-05-15 18:34:51 -0300

    add exercise 8

---
+++ src/main/java/com/hackerrank/java/introduction/Exercise8.java
@@ -0,0 +1,16 @@
+package com.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class Exercise8 {
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise8.txt");
+		try (Scanner scan = new Scanner(System.in);) {
+			int lineCounter = 1;
+			while (scan.hasNextLine()) {
+				String line = scan.nextLine();
+				System.out.println(lineCounter++ + " " + line);
+			}
+		}
+	}
+}
