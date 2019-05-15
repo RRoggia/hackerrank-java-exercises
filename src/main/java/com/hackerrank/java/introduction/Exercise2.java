336238e25c29172afef886a5651859fa10aae6e8
RRoggia <renanzr@gmail.com>
2019-05-14 22:47:31 -0300

    add exercise 2

---
+++ src/main/java/com/hackerrank/java/introduction/Exercise2.java
@@ -0,0 +1,34 @@
+package com.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class Exercise2 {
+
+	/*
+	 * Task Given an integer, N , perform the following conditional actions: If N is
+	 * odd, print Weird If N is even and in the inclusive range of 2 to 5, print Not
+	 * Weird If N is even and in the inclusive range of 6 to 20, print Weird If N is
+	 * even and greater than 20, print Not Weird
+	 */
+
+	private static final String NOT_WEIRD = "Not Weird";
+	private static final String WEIRD = "Weird";
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise2.txt");
+		try (Scanner scan = new Scanner(System.in)) {
+			while (scan.hasNextInt()) {
+				int value = scan.nextInt();
+
+				if (value % 2 != 0) {
+					System.out.println(WEIRD);
+				} else if ((value >= 2 && value <= 5) || value > 20) {
+					System.out.println(NOT_WEIRD);
+				} else if (value >= 6 && value <= 20) {
+					System.out.println(WEIRD);
+				}
+			}
+		}
+	}
+
+}
