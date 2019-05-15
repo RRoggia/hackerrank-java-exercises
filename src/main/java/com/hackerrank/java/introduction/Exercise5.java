a9433d791c12beb661495d663d5bb17ac8855f00
RRoggia <renanzr@gmail.com>
2019-05-14 23:37:52 -0300

    add exercise 5

---
+++ src/main/java/com/hackerrank/java/introduction/Exercise5.java
@@ -0,0 +1,22 @@
+package com.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class Exercise5 {
+	
+	/*
+	 * Print 10 lines of output; each line i (where 1 <= i <=10) contains the result of (N x i) in the form: N x i = result.
+	 */
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise5.txt");
+
+		try (Scanner scan = new Scanner(System.in);) {
+			int firstArgument = scan.nextInt();
+			for (int i = 1; i < 11; i++) {
+				System.out.println(firstArgument + " x " + i + " = " + firstArgument * i);
+			}
+		}
+	}
+
+}
