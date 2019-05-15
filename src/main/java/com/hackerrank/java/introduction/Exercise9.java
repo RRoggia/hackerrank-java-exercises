6e4d9c3c718ca0198206a44683b8addc28935a88
RRoggia <renanzr@gmail.com>
2019-05-15 18:45:02 -0300

    add exercise 9

---
+++ src/main/java/com/hackerrank/java/introduction/Exercise9.java
@@ -0,0 +1,30 @@
+package com.hackerrank.java.introduction;
+
+import java.util.Scanner;
+
+public class Exercise9 {
+
+	private static int B;
+	private static int H;
+	private static boolean flag = true;
+
+	static {
+		IOUtils.initializeInputStreamForFile("exercise9.txt");
+		try (Scanner scan = new Scanner(System.in);) {
+			B = scan.nextInt();
+			H = scan.nextInt();
+		}
+
+		if (B <= 0 || H <= 0) {
+			System.out.println("java.lang.Exception: Breadth and height must be positive");
+			flag = false;
+		}
+	}
+
+	public static void main(String[] args) {
+		if (flag) {
+			int area = B * H;
+			System.out.print(area);
+		}
+	}
+}
