33655d68e70a20a77984f2db0c5204c6a1685f4e
RRoggia <renanzr@gmail.com>
2019-05-16 13:24:30 -0300

    add exericse 18 - TODO improve regex

---
+++ src/main/java/com/hackerrank/java/strings/Exercise18.java
@@ -0,0 +1,29 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise18 {
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise18.txt");
+
+		try (Scanner scan = new Scanner(System.in)) {
+			String phrase = scan.nextLine();
+
+			String replace = phrase.replaceAll(" +", " ").trim();
+			if (replace.isEmpty()) {
+				System.out.println(0);
+				return;
+			}
+			String[] split = replace.split("[^a-zA-Z]+");
+
+			System.out.println(split.length);
+			for (String string : split) {
+				System.out.println(string);
+			}
+		}
+	}
+
+}
