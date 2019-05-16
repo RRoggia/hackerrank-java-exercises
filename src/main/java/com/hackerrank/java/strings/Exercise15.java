6cf4bc1f4d2b0974383c2aa72ce66cd8c0b6ad01
RRoggia <renanzr@gmail.com>
2019-05-15 23:56:25 -0300

    add exercise 15

---
+++ src/main/java/com/hackerrank/java/strings/Exercise15.java
@@ -0,0 +1,35 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise15 {
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise15.txt");
+		try (Scanner scan = new Scanner(System.in)) {
+			String word = scan.next();
+			int substringSize = scan.nextInt();
+			System.out.println(getSmallestAndLargest(word, substringSize));
+		}
+	}
+
+	public static String getSmallestAndLargest(String word, int size) {
+		String smallest = "";
+		String largest = "";
+
+		for (int i = 0; i <= word.length() - size; i++) {
+			String set = word.substring(i, i + size);
+
+			if (smallest.isEmpty() || smallest.compareTo(set) > 0)
+				smallest = set;
+
+			if (largest.isEmpty() || largest.compareTo(set) < 1)
+				largest = set;
+
+		}
+
+		return smallest + "\n" + largest;
+	}
+
+}
