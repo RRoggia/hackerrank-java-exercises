4a1e656f190dccb07f4b078476fe8d69369ada18
RRoggia <renanzr@gmail.com>
2019-05-16 00:04:34 -0300

    add exercise 16

---
+++ src/main/java/com/hackerrank/java/strings/Exercise16.java
@@ -0,0 +1,17 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise16 {
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise16.txt");
+		try (Scanner sc = new Scanner(System.in);) {
+			String A = sc.next();
+			StringBuilder sb = new StringBuilder(A);
+			System.out.println(A.equals(sb.reverse().toString()) ? "Yes" : "No");
+		}
+	}
+}
