23ec7fd3887def85520547a26aacec88b87507fc
RRoggia <renanzr@gmail.com>
2019-05-15 23:39:25 -0300

    add exercise 13

---
+++ src/main/java/com/hackerrank/java/strings/Exercise13.java
@@ -0,0 +1,24 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise13 {
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise13.txt");
+		try (Scanner scan = new Scanner(System.in)) {
+			String firstString = scan.next();
+			String secondString = scan.next();
+
+			System.out.println(firstString.length() + secondString.length());
+			System.out.println(firstString.compareTo(secondString) > 0 ? "Yes" : "No");
+			StringBuilder sb = new StringBuilder();
+			String string = sb.append(firstString.substring(0, 1).toUpperCase())
+					.append(firstString.substring(1, firstString.length())).append(" ")
+					.append(secondString.substring(0, 1).toUpperCase())
+					.append(secondString.substring(1, secondString.length())).toString();
+			System.out.println(string);
+		}
+	}
+}
