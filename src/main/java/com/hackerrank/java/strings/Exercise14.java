de6888547af8644e0a511c82f378c8645baaea02
RRoggia <renanzr@gmail.com>
2019-05-15 23:56:19 -0300

    add exercise 14

---
+++ src/main/java/com/hackerrank/java/strings/Exercise14.java
@@ -0,0 +1,19 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise14 {
+	public static void main(String[] args) {
+
+		IOUtils.initializeInputStreamForFile("exercise14.txt");
+		try (Scanner scan = new Scanner(System.in)) {
+			String string = scan.next();
+			int start = scan.nextInt();
+			int end = scan.nextInt();
+			System.out.println(string.substring(start, end));
+		}
+
+	}
+}
