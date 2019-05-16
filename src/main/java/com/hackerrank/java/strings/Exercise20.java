5e1d1927aec9cbefe6b680254c0b4ff1b327be7f
RRoggia <renanzr@gmail.com>
2019-05-16 19:26:48 -0300

    add exercise 20

---
+++ src/main/java/com/hackerrank/java/strings/Exercise20.java
@@ -0,0 +1,22 @@
+package com.hackerrank.java.strings;
+
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise20 {
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise20.txt");
+		try (Scanner in = new Scanner(System.in);) {
+			while (in.hasNext()) {
+				String IP = in.next();
+				System.out.println(IP.matches(new MyRegex().pattern));
+			}
+		}
+	}
+
+}
+
+class MyRegex {
+	public String pattern = "(([0-2][0-5][0-5]|2[0-4][0-9]|0?[0-9][0-9]|0?0?[0-9])\\.){3}([0-2][0-5][0-5]|2[0-4][0-9]|0?[0-9][0-9]|0?0?[0-9])$";
+}
