502682e5a1013d660da99a3f6217a4f86f5ccf3d
RRoggia <renanzr@gmail.com>
2019-05-17 16:12:01 -0300

    add exercise 30

---
+++ src/main/java/com/hackerrank/java/exception/Exercise30.java
@@ -0,0 +1,24 @@
+package com.hackerrank.java.exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise30 {
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise30.txt");
+		try (Scanner scan = new Scanner(System.in)) {
+			int x = scan.nextInt();
+			int y = scan.nextInt();
+
+			System.out.println(x / y);
+		} catch (InputMismatchException e) {
+			System.out.println("java.util.InputMismatchException");
+		} catch (ArithmeticException e) {
+			System.out.println("java.lang.ArithmeticException: / by zero");
+		}
+	}
+
+}
