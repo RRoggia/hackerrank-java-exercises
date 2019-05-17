76ff9cd94a0abc133c2b0fa1d52586567a5f32c8
RRoggia <renanzr@gmail.com>
2019-05-17 16:24:56 -0300

    add exercise 31

---
+++ src/main/java/com/hackerrank/java/exception/Exercise31.java
@@ -0,0 +1,37 @@
+package com.hackerrank.java.exception;
+
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise31 {
+	public static final MyCalculator my_calculator = new MyCalculator();
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise31.txt");
+		try (Scanner scan = new Scanner(System.in);) {
+			while (scan.hasNextInt()) {
+				int n = scan.nextInt();
+				int p = scan.nextInt();
+
+				try {
+					System.out.println(my_calculator.power(n, p));
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+		}
+	}
+
+}
+
+class MyCalculator {
+	public int power(int n, int p) throws Exception {
+		if (n < 0 || p < 0) {
+			throw new Exception("n or p should not be negative.");
+		} else if (n == 0 && p == 0) {
+			throw new Exception("n and p should not be zero.");
+		}
+		return (int) Math.pow(n, p);
+	}
+}
