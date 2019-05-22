118f979b1ab6d76dea1d0b10f4ed727115061225
RRoggia <renanzr@gmail.com>
2019-05-22 14:33:28 -0300

    add exercise 36

---
+++ src/main/java/com/hackerrank/java/datastructure/Exercise36.java
@@ -0,0 +1,42 @@
+package com.hackerrank.java.datastructure;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.hackerrank.java.introduction.IOUtils;
+
+public class Exercise36 {
+
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise36.txt");
+		try (Scanner scan = new Scanner(System.in)) {
+			int numberOfIntegers = scan.nextInt();
+
+			ArrayList<Integer> integers = new ArrayList<>();
+			for (int i = 0; i < numberOfIntegers; i++) {
+				integers.add(scan.nextInt());
+			}
+
+			int numberOfQueries = scan.nextInt();
+
+			for (int i = 0; i < numberOfQueries; i++) {
+				String next = scan.next();
+				int index = 0;
+				switch (next) {
+				case "Insert":
+					index = scan.nextInt();
+					int number = scan.nextInt();
+					integers.add(index, number);
+					break;
+				case "Delete":
+					index = scan.nextInt();
+					integers.remove(index);
+					break;
+				}
+			}
+
+			integers.forEach(number -> System.out.print(number + " "));
+		}
+	}
+
+}
