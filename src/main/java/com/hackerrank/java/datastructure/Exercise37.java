5e53c2bae907e034d9760921da047fa352017983
RRoggia <renanzr@gmail.com>
2019-06-05 19:00:47 -0300

    add exercise 37

---
+++ src/main/java/com/hackerrank/java/datastructure/Exercise37.java
@@ -0,0 +1,33 @@
+package com.hackerrank.java.datastructure;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import com.hackerrank.java.IOUtils;
+
+public class Exercise37 {
+	public static void main(String[] args) {
+		IOUtils.initializeInputStreamForFile("exercise37.txt");
+		try (Scanner scan = new Scanner(System.in);) {
+			int numberOfPerson = scan.nextInt();
+			scan.nextLine();
+
+			Map<String, Integer> phoneBook = new HashMap<>();
+			for (int i = 0; i < numberOfPerson; i++) {
+				String name = scan.nextLine();
+				int phone = scan.nextInt();
+				phoneBook.put(name, phone);
+				scan.nextLine();
+			}
+			while (scan.hasNext()) {
+				String name = scan.nextLine();
+				if (!phoneBook.containsKey(name)) {
+					System.out.println("Not found");
+				} else {
+					System.out.println(name + "=" + phoneBook.get(name));
+				}
+			}
+		}
+	}
+}
